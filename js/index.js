8d5f5617afd00339b4f9ca11490b16ab3176bb90
VincentKempers <me@vncn.tk>
2020-07-27 12:56:20 +0200

    add what you see is what you get rule:

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 console.log('connected');
+arr = ['1','2','3','4']
+
+
+function arrayLoop(arrayThing) {
+  console.log('connected');
+  arrayThing.forEach((item) => {
+    console.log(item);
+  })
+}
+
+
