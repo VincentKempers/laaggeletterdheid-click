dfda217bf302730ff8124e1771df4df0f8b9669b
VincentKempers <me@vncn.tk>
2020-08-03 20:29:06 +0200

    setup nuxt-vue-graphql

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Hello World!</h1>
+    <p class="part--thing" @click="blue"> green thing</p>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+
+<style lang="less">
+
+.part--thing {
+  color : red;
+}
+
+</style>
