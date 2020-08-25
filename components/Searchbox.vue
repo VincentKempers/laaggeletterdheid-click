5bc6abe16be746a9ec0080bb7128d3b058e3ca09
VincentKempers <me@vncn.tk>
2020-08-25 19:41:42 +0200

    add searchbox file

---
+++ components/Searchbox.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="searchBar">
+    <input type="text">
+    <img src="" alt="Search logo">
+  </div>
+</template>
+
+<style>
+.NuxtLogo {
+  animation: 1s appear;
+  margin: auto;
+}
+
+@keyframes appear {
+  0% {
+    opacity: 0;
+  }
+}
+</style>
